781864dc5c6b88aee5e939d515ee9c90f38fa8ba
rqh656418510 <656418510@qq.com>
2022-06-20 10:33:22 +0800

    feat: optimize log message output

--- src/node/utils.ts
+++ src/node/utils.ts
@@ -1,13 +1,13 @@
 import * as chalk from 'chalk';
 
 export const info = (msg, textColor = 'white', bgColor = 'bgBlue', label = 'BAIDU SEO PLUGIN') => {
-    console.log(`${chalk.reset.bold[textColor][bgColor](` ${label}`)} ${msg}`);
+    console.log(`${chalk.reset.bold[textColor][bgColor](` ${label} `)} ${msg}`);
 }
 
 export const warn = (msg, textColor = 'white', bgColor = 'bgYellow', label = 'BAIDU SEO PLUGIN') => {
-    console.log(`${chalk.reset.bold[textColor][bgColor](` ${label}`)} ${msg}`);
+    console.log(`${chalk.reset.bold[textColor][bgColor](` ${label} `)} ${msg}`);
 }
 
 export const error = (msg, textColor = 'white', bgColor = 'bgRed', label = 'BAIDU SEO PLUGIN') => {
-    console.log(`${chalk.reset.bold[textColor][bgColor](` ${label} ${msg}`)}`);
+    console.log(`${chalk.reset.bold[textColor][bgColor](` ${label}  ${msg}`)}`);
 }
