cb0a669ca9e7b79ee3e5552442e212b9ff2c94a1
rqh656418510 <656418510@qq.com>
2022-07-11 21:35:11 +0800

    fix: Fix repeat submit for baidu tongji

--- src/client/composables/useBaiduSeoPlugin.ts
+++ src/client/composables/useBaiduSeoPlugin.ts
@@ -6,29 +6,62 @@ declare global {
   interface Window { _hmt: any; }
 }
 
+// 初始化百度SEO插件的配置参数
 declare const __BAIDU_SEO_OPTIONS__: BaiduSeoOptions;
 const options = __BAIDU_SEO_OPTIONS__;
 const IGNORE_LOCAL = options.ignoreLocal;
 const HM = options.hm;
 
-/**
- * baidu tongji
- * @returns 
- */
-function initBaiduTongji() {
-  if (HM && typeof window !== 'undefined') {
+// 使用百度SEO插件
+export const useBaiduSeo = (): void => {
 
+  onMounted(() => {
+    // 百度统计提交
+    tongjiSubmit(1000);
+
+    // 监听路由变化
+    const router = useRouter();
+    router.afterEach((to, from) => {
+      var toPath = to.path;
+      var fromPath = from.path;
+      // 忽略带锚点的路由变化
+      if (toPath != fromPath) {
+        tongjiSubmit(500);
+      }
+    });
+  })
+
+}
+
+/**
+ * 百度统计提交
+ */
+function tongjiSubmit(waitMills: number) {
+  // 利用定时器来保证可以正常操作DOM节点
+  setTimeout(() => {
+    tongjiPush();
+  }, waitMills);
+}
+
+/**
+ * 百度统计推送
+ */
+function tongjiPush() {
+  if (HM && typeof window !== 'undefined') {
+    // 忽略本地的访问记录
     const host = window.location.host;
     if ((host.indexOf("127.0.0.1") != -1 || host.indexOf("localhost") != -1) && IGNORE_LOCAL) {
       return;
     }
 
+    // 删除已存在的DOM节点
     const id = 'baidu-tongji';
     var element = document.getElementById(id);
     if (element) {
       element.remove();
     }
 
+    // 推送访问记录
     window._hmt = window._hmt || [];
     const hm = document.createElement('script');
     hm.id = id;
@@ -36,29 +69,7 @@ function initBaiduTongji() {
     const s = document.getElementsByTagName('script')[0];
     if (s.parentNode) {
       s.parentNode.insertBefore(hm, s);
+      window._hmt.push(['_trackPageview', window.location.href]);
     }
-
-    const router = useRouter();
-    const loadedPages = new Set();
-
-    router.afterEach((to) => {
-      // resolve duplicate requests in a short time
-      if (loadedPages.has(to.fullPath)) {
-        setTimeout(() => {
-          loadedPages.delete(to.fullPath);
-        }, 1000);
-      } else {
-        loadedPages.add(to.fullPath);
-        window._hmt.push(['_trackPageview', to.fullPath]);
-      }
-    });
   }
-}
-
-export const useBaiduSeo = (): void => {
-
-  onMounted(() => {
-    initBaiduTongji();
-  })
-
 }
